5a4eb7356ba099e8b5004cfab92bf1e503080e85
Mikkel Ruby <143700888+Mikkelrubydk@users.noreply.github.com>
2024-05-14 09:38:45 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const menuBtn = document.querySelector('.menu-btn');
+const burgerIndhold = document.querySelector('.burgerindhold');
+const centerLogo = document.querySelector('.centerlogo');
+const pilNed = document.querySelector('.pilned');
+let menuOpen = false;
+
+document.addEventListener('scroll', () => {
+    let nav = document.querySelector(`header nav`);
+  
+    if (window.scrollY === 0) {
+      nav.classList.add(`scrolled`);
+    } else {
+      nav.classList.remove('scrolled');
+    }
+  });
+
+
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+      menuBtn.classList.add('open');
+      menuOpen = true;
+      burgerIndhold.style.right = "0";
+      centerLogo.style.transition = "opacity 1s";
+      pilNed.style.transition = "opacity 1s";
+      centerLogo.style.opacity = "0";
+      burgerIndhold.style.animation = "fade-in 1s forwards";
+      burgerIndhold.style.opacity = "1";
+      pilNed.style.opacity = "0";
+    } else {
+      menuBtn.classList.remove('open');
+      menuOpen = false;
+      burgerIndhold.style.right = "-100%";
+      centerLogo.style.transition = "opacity 1s";
+      centerLogo.style.opacity = "1";
+      pilNed.style.opacity = "1";
+      pilNed.style.transition = "opacity 1s";
+      burgerIndhold.style.animation = "fade-out 1s forwards";
+      burgerIndhold.style.opacity = "0";
+    }
+  });
+
+pilNed.addEventListener('click', function() {
+  window.scrollTo({
+    top: window.innerHeight,
+    behavior: 'smooth'
+  });
+});
+
+document.querySelectorAll('.flip-card').forEach(function(card) {
+    card.addEventListener('click', function() {
+        card.querySelector('.flip-card-inner').classList.toggle('flipped');
+    });
+});
+
