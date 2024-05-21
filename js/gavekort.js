3a7390a7d049b01486e03057928d63b309b55c72
MikkelRuby <mikkelruby1@hotmail.com>
2024-05-21 11:45:41 +0200

    fwfw

--- js/gavekort.js
+++ js/gavekort.js
@@ -25,35 +25,3 @@ function updateAmount() {
     giftcardAmount.textContent = `${currentAmount} DKK`;
 }
 
-
-// laver billede serie, man kan klikke sig igennem 
-document.addEventListener('DOMContentLoaded', () => {
-    const leftArrow = document.querySelector('.pil-venstre');
-    const rightArrow = document.querySelector('.pil-højre');
-    const imageContainer = document.querySelector('.billede-container');
-    const images = document.querySelectorAll('.billede-container picture');
-    const visibleImages = 4;
-    let currentIndex = 0;
-
-    function updateScroll() {
-        const imageWidth = images[0].getBoundingClientRect().width + 10; // Tilføjer + 10 da der er et 10px gap mellem billederne
-        const maxIndex = images.length - visibleImages;
-        if (currentIndex < 0) {
-            currentIndex = maxIndex;
-        } else if (currentIndex > maxIndex) { // Sørger for at når billed-slideren starter ved det første billede, når den når sit maxindex.
-            currentIndex = 0;
-        }
-        const newTransform = `translateX(-${currentIndex * imageWidth}px)`;
-        imageContainer.style.transform = newTransform;
-    }
-
-    leftArrow.addEventListener('click', () => {
-        currentIndex -= 1; // Rykker et billede til venstre
-        updateScroll();
-    });
-
-    rightArrow.addEventListener('click', () => {
-        currentIndex += 1; // Rykker et billede til højre
-        updateScroll();
-    });
-});
