8f095bc0ae8005efd545c7d2663e355eeedab926
MikkelRuby <mikkelruby1@hotmail.com>
2024-05-23 09:02:45 +0200

    rettelser

--- js/404.js
+++ js/404.js
@@ -1,4 +1,3 @@
-
 // Tilføjelse og fjernelse af elementer/effekter når brugermenuen er hhv. lukket og åben.
 
 const menuBtn = document.querySelector('.menu-btn');
@@ -31,31 +30,3 @@ menuBtn.addEventListener('click', () => {
       burgerIndhold.style.opacity = "0";
     }
   });
-
-// gør sådan at man kan ændre beløbet når man klikker på plus og mins 
-
-const giftcardAmount = document.getElementById('gavekort-beløb');
-const plusBtn = document.getElementById('plus');
-const minusBtn = document.getElementById('minus');
-
-let currentAmount = 500; // Start beløbet er 500kr
-
-plusBtn.addEventListener('click', function() {
-    if (currentAmount < 2000) { // sikre at beløbet er maks 2000 dkk 
-        currentAmount += 100;
-        updateAmount();
-    }
-});
-
-minusBtn.addEventListener('click', function() {
-    if (currentAmount > 100) { // sikre at beløber ikke går under 100 
-        currentAmount -= 100;
-        updateAmount();
-    }
-});
-
-
-function updateAmount() {
-    giftcardAmount.textContent = `${currentAmount} DKK`;
-}
-
--- js/event.js
+++ js/event.js
@@ -1,4 +1,37 @@
 
+// Tilføjelse og fjernelse af elementer/effekter når brugermenuen er hhv. lukket og åben.
+
+const menuBtn = document.querySelector('.menu-btn');
+const burgerIndhold = document.querySelector('.burgerindhold');
+const centerLogo = document.querySelector('.centerlogo');
+const pilNed = document.querySelector('.pilned');
+let menuOpen = false;
+
+
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+      menuBtn.classList.add('open');
+      menuOpen = true;
+      burgerIndhold.style.right = "0";
+      centerLogo.style.transition = "opacity 1s";
+      pilNed.style.transition = "opacity 1s";
+      centerLogo.style.opacity = "0";
+      burgerIndhold.style.animation = "fade-in .5s forwards";
+      burgerIndhold.style.opacity = "1";
+      pilNed.style.opacity = "0";
+    } else {
+      menuBtn.classList.remove('open');
+      menuOpen = false;
+      burgerIndhold.style.right = "-100%";
+      centerLogo.style.transition = "opacity 1s";
+      centerLogo.style.opacity = "1";
+      pilNed.style.opacity = "1";
+      pilNed.style.transition = "opacity 1s";
+      burgerIndhold.style.animation = "fade-out 1s forwards";
+      burgerIndhold.style.opacity = "0";
+    }
+  });
+
 if (window.matchMedia('(max-width: 1080px)').matches) {
     // Sørger for at koden kun kører, når brugeren er på mobilen
     document.querySelectorAll('.flip-card-event').forEach(function(card) {
